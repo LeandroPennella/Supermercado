47e4e5fc3d5752764f1f446838d3b90ced2ddae0
Leandro Pennella <leandropennella@yahoo.com.ar>
2019-08-15 15:49:08 -0300

    Agregar archivos de proyecto.

---
+++ Modelo/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public int codigo{ get; set; }
+        public string descripcion{ get; set; }
+    }
+
+    public class ProductoStock { 
+        public Producto producto { get; set; }
+        public DateTime ingreso { get; set; }
+        public DateTime egreso{get; set;}
+    }
+
+    //precio?
+    public class Stock
+    { 
+        List<Producto> productos { get; set; }
+    }
+
+    public class Venta
+    {
+        public int id { get; set; }
+        public DateTime fecha { get; set; }
+        public List<Producto> Productos { get;set;}
+        public EnumEstado Estado { get;set; }
+    }
+
+    public enum EnumEstado { 
+        abierta, consumada, cancelada
+    }
+}
